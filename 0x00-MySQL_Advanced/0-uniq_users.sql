01183ae3cf2369cafae9571564fe29c9ba287425
murewaabigal <murewaabigal@gmail.com>
2023-01-15 05:33:40 +0000

    commit

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS users;
 
-
--- Creates a table with unique users.DROP TABLE IF EXISTS users;CREATE TABLE users (    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,    email VARCHAR(255) NOT NULL UNIQUE,    name VARCHAR(255));
+CREATE TABLE
+    users (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255)
+    );
